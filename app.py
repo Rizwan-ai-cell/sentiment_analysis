ad0fc2d3ee245710d6344d9a1844b87c4d5cc3e6
Rizwan-ai-cell <amrizwan175@gmail.com>
2024-11-18 14:27:04 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from tensorflow.keras.models import load_model # type: ignore
-from tensorflow.keras.preprocessing.sequence import pad_sequences # type: ignore
+import tensorflow as tf
+from tensorflow.keras.models import load_model  # type: ignore
+from tensorflow.keras.preprocessing.sequence import pad_sequences  # type: ignore
 import pickle
 
 # Load the trained model and tokenizer
